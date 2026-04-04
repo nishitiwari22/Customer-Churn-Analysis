238f97df334174ba0c02785ed21b94393b1037a0
Nishi Tiwari <90550117+nishitiwari22@users.noreply.github.com>
2026-04-04 19:01:17 +0530

    Add files via upload

---
+++ app/streamlit_app.py
@@ -0,0 +1,13 @@
+python --versionimport streamlit as st
+import pickle
+
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("Customer Churn Predictor")
+
+tenure = st.slider("Tenure", 1, 72)
+monthly_charges = st.number_input("Monthly Charges")
+
+if st.button("Predict"):
+    prediction = model.predict([[tenure, monthly_charges]])
+    st.write("Churn Risk:", prediction)
